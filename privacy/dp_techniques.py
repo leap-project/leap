39a47e1a911f4ec3e36e3aa708cde277e6bd246d
Muhammad Shayan <shayan@naur.cs.ubc.ca>
2019-05-28 19:05:17 -0700

    Privatized sensitive count + mean + variance

--- privacy/dp_techniques.py
+++ privacy/dp_techniques.py
@@ -1,29 +1,132 @@
 import numpy as np
 
+
 def dp_count(count, epsilon=1.0, delta=0.1):
 
-    """
-    This function provides a differentially-private estimate of a count.
+		"""
+		This function provides a differentially-private estimate of a count. Here each user's record affects by 1
 
-    Input:
+		Input:
 
-      count = The value of count that needs to be privatized based on an epsilon.
-      epsilon = privacy parameter, default 1.0
-      delta = privacy parameter, default 0.1
+			count = The value of count that needs to be privatized based on an epsilon.
+			epsilon = privacy parameter, default 1.0
+			delta = privacy parameter, default 0.1
 
-    Output:
+		Output:
 
-      Privatized count
+			Privatized count
 
-    """
-    COUNT_SENSITIVITY = 1  # Global sensitivity of a simple count query
-    
-    if delta == 0:
-      noise = np.random.laplace(loc = 0, scale = COUNT_SENSITIVITY/float(epsilon), size = (1,1))
-    else:    
-      sigma = (COUNT_SENSITIVITY/(epsilon))*np.sqrt(2*np.log(1.25/delta))
-      noise = np.random.normal(0.0, sigma, 1)
+		"""
+		COUNT_SENSITIVITY = 1  # Global sensitivity of a simple count query
+		
+		if delta == 0:
+			noise = np.random.laplace(loc = 0, scale = COUNT_SENSITIVITY/float(epsilon), size = (1,1))
+		else:    
+			sigma = (COUNT_SENSITIVITY/(epsilon))*np.sqrt(2*np.log(1.25/delta))
+			noise = np.random.normal(0.0, sigma, 1)
 
-    count += noise
+		count += noise
 
-    return int(count)
+		return int(count)
+
+def dp_sensitive_count(sensitivity, count, epsilon=1.0, delta=0.1):
+
+		"""
+		This function provides a differentially-private estimate of a count where the effect of one user's data is greater than one on the count.
+
+		Input:
+
+			sensitivity = The max amount by which one user's data can change the count. 
+			count = The value of count that needs to be privatized based on an epsilon.
+			epsilon = privacy parameter, default 1.0
+			delta = privacy parameter, default 0.1
+
+		Output:
+			Privatized count
+		"""
+		COUNT_SENSITIVITY = sensitivity
+		if delta == 0:
+			noise = np.random.laplace(loc = 0, scale = COUNT_SENSITIVITY/float(epsilon), size = (1,1))
+		else:    
+			sigma = (COUNT_SENSITIVITY/(epsilon))*np.sqrt(2*np.log(1.25/delta))
+			noise = np.random.normal(0.0, sigma, 1)
+
+		count += noise
+
+		return int(count)
+
+def dp_mean(mean_min, mean_max,num_samples, mean, epsilon=1.0, delta=0.1):
+
+	"""
+		This function provides a differentially-private estimate of the mean of a vector.
+
+		Input:
+
+			mean = Mean to be privatized
+			sensitivity = Max of the range of values used to compute the mean 
+			num_samples = number if samples used to calculate the mean
+			epsilon = privacy parameter, default 1.0
+			delta = privacy parameter, default 0.1
+
+		Output:
+
+			a scalar.
+
+	 """
+	n = num_samples
+	f = mean
+	MEAN_SENSITIVITY = (mean_max - mean_min)/n
+
+	if delta == 0:
+			noise = np.random.laplace(loc = 0, scale = MEAN_SENSITIVITY/float(epsilon), size = (1,1))
+	else:      
+			sigma = (MEAN_SENSITIVITY/(epsilon))*np.sqrt(2*np.log(1.25/delta))
+			noise = np.random.normal(0.0, sigma, 1)
+
+	f += noise
+	return f
+
+def dp_variance(var_min, var_max,  num_samples, variance, epsilon=1.0, delta=0.1):
+
+	"""
+		This function provides a differentially-private estimate of the mean of a vector.
+
+		Input:
+
+			var = Variance to be privatized
+			var_max = Max of the range of values used to compute variance 
+			var_min = Min of the range of values used to compute the variance
+
+			num_samples = number if samples used to calculate the mean
+			epsilon = privacy parameter, default 1.0
+			delta = privacy parameter, default 0.1
+
+		Output:
+
+			a scalar.
+
+	 """
+	f = variance
+	n = num_samples   
+
+	VAR_SENSITIVITY = ((var_max - var_min)**2)/n
+
+	if delta == 0:
+			noise = np.random.laplace(loc = 0, scale = VAR_SENSITIVITY/float(epsilon), size = (1,1))
+	else:      
+			sigma = (VAR_SENSITIVITY/(epsilon))*np.sqrt(2*np.log(1.25/delta))
+			noise = np.random.normal(0.0, sigma, 1)
+
+	f += noise
+
+	return f
+
+
+
+
+
+
+
+
+
+
--- privacy/exampleCode.py
+++ privacy/exampleCode.py
@@ -3,10 +3,57 @@ import dp_techniques as dps
 
 
 #Privacy parameters
-epsilon = 0.1
+epsilon = 0.5
 delta = 0
 
-# Example for privatizing counting
+# Example for privatizing simple counts 
 orig_count = 20
 privatized_count = dps.dp_count(orig_count, epsilon, delta)
 print("Original count: %d, Privatized count: %d, Epsilon: %f" % (orig_count, privatized_count, epsilon))
+
+# Example for privatizing more sensitive counts
+orig_count = 20
+count_sensitivity=5 # how much can a record change the record
+privatized_count = dps.dp_sensitive_count(count_sensitivity, orig_count, epsilon, delta)
+print("Original count: %d with sensitivity: %d, Privatized count: %d, Epsilon: %f" % (orig_count,count_sensitivity, privatized_count, epsilon))
+
+# # Example for privatizing means
+data_vec = np.random.rand(10) # values in the range 0,1
+data_max = 1
+data_min = 0
+n = len(data_vec)
+avg = np.mean(data_vec)
+privatized_mean = dps.dp_mean(data_min, data_max, n, avg, epsilon, delta)
+print("Original data: %s, Privatized mean: %f, Epsilon: %f" % (str(data_vec), privatized_mean, epsilon))
+
+#Example for privatizing variance
+
+data_vec = np.random.rand(10) # values in the range 0,1
+data_max = 1.0
+data_min = 0.0
+n = len(data_vec)
+avg = np.mean(data_vec)
+var = (1.0/n) * sum((value - avg) ** 2 for value in data_vec)
+
+privatized_var = dps.dp_variance(data_min,data_max,n, var,epsilon, delta)
+print("Original data: %s , Privatized variance: %f, Epsilon: %f" % (str(data_vec), privatized_var, epsilon))
+
+# Example for privatizing median
+data_vec = np.random.rand(10) # values in the range 0,1
+data_max = 1.0
+data_min = 0.0
+
+
+
+
+# Example for privatizing median
+
+
+#Example for privatizing linear regression
+
+
+
+
+
+
+

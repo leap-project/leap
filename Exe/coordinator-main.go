6b4639e19f222141b5b2f276d68407b998f6ef82
Matt <matheusstolet@hotmail.com>
2019-06-24 16:19:24 -0700

    New Exe files

---
+++ Exe/coordinator-main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"leap/Coordinator"
+)
+
+// Run with go run *.go
+//
+// Accepted Flags
+// - ip: Ip and port to listen for connections
+//
+// e.g go run *.go -ip=127.0.0.1:8000
+
+func main() {
+	config := coordinator.GetConfig("coord-config.json")
+	coordinator.AddFileHookToLogs("Logs/")
+	coord := coordinator.NewCoordinator(config)
+	go coord.Serve()
+	// Sleep main goroutine forever
+	select {}
+}

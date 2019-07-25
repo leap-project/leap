80acd09cb19597e9409738e410bd5b4d9daf728e
Chris <chris.jm.yoon@gmail.com>
2019-07-25 14:34:44 -0700

    Implemented part of cloud algo and client connector and tested up to calling coordinator.map

--- CloudAlgos/client_connector.py
+++ CloudAlgos/client_connector.py
@@ -13,66 +13,34 @@ import ProtoBuf as pb
 # pb.computation_msgs_pb2
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-id", "--algoId", default=10, help="The id of this algorithm")
-parser.add_argument("-cip", "--coordinatorIpPort", default='127.0.0.1:50000', help="The ip and port of the coordinator")
+parser.add_argument("-cip", "--cloudAlgosIpPort", default='127.0.0.1:60000', help="The ip and port of the cloudAlgos")
 args = parser.parse_args()
 
 
 class ClientConnector():
     def __init__(self, ipPort):
         print("Initialized Client Connector")
-        self.coordinatorIpPort = ipPort
+        self.cloudAlgosIpPort = ipPort
 
 
-    def _create_computation_request(self, u_module, query):
+    def _create_computation_request(self, u_module, filter):
         request = pb.computation_msgs_pb2.ComputeRequest()
-        request.algo_id = int(args.algoId)
         req = {}
         req["module"] = u_module
-        req["query"] = query
+        req["filter"] = filter
         request.req = json.dumps(req)
         return request
 
 
-    # Creates a cloud algo registration request using the parameters
-    # given.
-    #
-    # id: The id of the algorithm to be registered.
-    # description: A description of what this algorithm does.
-    # proto_version: The protocol buffer version being used by
-    #                this algorithm.
-    def _create_registration_request(self, id, description, proto_version):
-        request = pb.registration_msgs_pb2.CloudAlgoRegReq()
-        request.id = id
-        request.description = description
-        request.proto_version = proto_version
-        return request
-
-    # Makes an RPC call to the coordinator to register an algo
-    # with the id given as parameter.
-    #
-    # stub: Stub for the cloud coordinator.
-    # algo_id: The id of the algo to be registered.
-    def _register(self, stub, algo_id):
-        description = "A generic UDF algorithm"
-        registration_request = self._create_registration_request(algo_id, description, "proto3")
-        response = stub.RegisterCloudAlgo(registration_request)
-        if hasattr(response, "success"):
-            if response.success:
-                print(response.msg)
-            else:
-                print(response.msg)
-
-    def send_request(self, algo_id, u_module, query):
+    def send_request(self, u_module, filter):
+        print("Sending request from client_connector")
         # Sets up the connection so that we can make RPC calls
-        with grpc.insecure_channel(args.coordinatorIpPort) as channel:
-            stub = pb.coordinator_pb2_grpc.CoordinatorStub(channel)
+        with grpc.insecure_channel(args.cloudAlgosIpPort) as channel:
+            stub = pb.cloud_algos_pb2_grpc.CloudAlgoStub(channel)
 
-            self._register(stub, int(algo_id))
-            print("Query: {}".format(query))
+            print("Filter: {}".format(filter))
 
-
-            req = self._create_computation_request(u_module, query)
+            req = self._create_computation_request(u_module, filter)
 
             result = stub.Compute(req) # Computed remotely
 
@@ -89,13 +57,12 @@ class ClientConnector():
 
 def client_request():
     # Create connector. TODO: Decide how client request will talk to connector
-    connector = ClientConnector(args.coordinatorIpPort)
+    connector = ClientConnector(args.cloudAlgosIpPort)
 
     # Get source code for map, agg, update, etc
     module = inspect.getsource(count_fn)
-    query = "[age] > 50 and [bmi] < 25"
-    algoId = args.algoId
-    connector.send_request(algoId, module, query)
+    filter = "[age] > 50 and [bmi] < 25"
+    connector.send_request(module, filter)
     pdb.set_trace()
 
 
--- CloudAlgos/cloud_algo.py
+++ CloudAlgos/cloud_algo.py
@@ -1,1 +1,100 @@
-package CloudAlgos
+"""
+Server to listen to requests from the Coordinator and 
+spawn processes responsible for iterative aggregation logic
+
+1. Generate unique ID for UDF
+2. grpc call to coordinator to get site map results
+"""
+import time
+import argparse
+import multiprocessing
+import grpc
+import concurrent.futures as futures
+import pdb
+import json
+import sys
+sys.path.append("../")
+
+import ProtoBuf as pb
+parser = argparse.ArgumentParser()
+parser.add_argument("-ip", "--ipPort", default="127.0.0.1:60000", help="The ip and port this algorithm is listening to")
+parser.add_argument("-cip", "--coordinatorIpPort", default="127.0.0.1:50001", help="The ip and port of the cloud coordinator")
+args = parser.parse_args()
+
+# Receives ComputeRequest from Coordinator
+class CloudAlgoServicer(pb.cloud_algos_pb2_grpc.CloudAlgoServicer):
+    def __init__(self, ipPort, coordinatorIpPort):        
+        self.ipPort = ipPort
+        self.coordinatorIpPort = coordinatorIpPort
+        
+
+        # super(CloudAlgoServicer, self).__init__() # Object ... doesn't do anythin
+        self.id_to_thread = {}
+        self.id_count = 0
+
+    def _create_computation_request(self, req_id, req):
+        request = pb.computation_msgs_pb2.MapRequest()
+        request.id = req_id
+        # TODO: Split cloud algo udf functions from site algos
+        request.req = json.dumps(req)
+        return request
+
+    def _generate_req_id(self):
+        # TODO: actually spawn child processes. thread is currently None
+        self.id_to_thread[self.id_count] = None
+        self.id_count += 1
+        return self.id_count
+
+    def Compute(self, request, context):
+        print("Cloud-Algo : Got compute call")
+        # TODO: This logic should eventually be ran in separate thread
+        req = json.loads(request.req)
+        print("Loading module...")
+        exec(req["module"], globals())
+        # print("Request in cloud_algo.Compute: {}".format(req))
+        state = globals()["state"]
+        stop = False
+        
+        # Generate algo_id
+        req_id = self._generate_req_id()
+        print("Generated cloud algo id: {}".format(req_id))
+        with grpc.insecure_channel(self.coordinatorIpPort) as channel:
+            coord_stub = pb.coordinator_pb2_grpc.CoordinatorStub(channel)
+            print("Created coordinator stub")
+            while not stop:
+                map_results = []
+                choice = choice_fn(state)
+                print("Choice: {}".format(choice))
+                request = self._create_computation_request(req_id, req)               
+                print("Created map request")
+
+                result = coord_stub.Map(request) # Computed remotely
+                print("Received map results from cloud coordinator")
+
+                map_results = json.loads(result.response)
+                agg_result = agg_fn[choice](map_results)
+                state = update_fn[choice](agg_result, state)
+                stop = stop_fn(agg_result, state)
+            
+            res = pb.computation_msgs_pb2.ComputeResponse()
+            res.response = json.dumps(post_fn(agg_result, state))
+        print("returning response to client connector")
+        return res
+
+# Starts listening for RPC requests at the specified ip and
+# port.
+#
+# No args
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb.cloud_algos_pb2_grpc.add_CloudAlgoServicer_to_server(CloudAlgoServicer(args.ipPort, args.coordinatorIpPort), server)
+    server.add_insecure_port(args.ipPort)
+    server.start()
+    print("Cloud Algo : Server started")
+    print("Cloud Algo : Listening at {}".format(args.ipPort))
+    while True:
+        time.sleep(1)
+
+if __name__ == "__main__":
+    serverProcess = multiprocessing.Process(target=serve)
+    serverProcess.start()

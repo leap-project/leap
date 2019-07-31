56cb22da5f38efc804e2dcb94d329546a7703125
Matt <matheusstolet@hotmail.com>
2019-07-31 16:51:08 -0700

    Better handling of unavailable sites

--- ProtoBuf/compileProtos.sh
+++ ProtoBuf/compileProtos.sh
@@ -17,10 +17,6 @@ python -m grpc_tools.protoc -I=. --python_out=. --grpc_python_out=. coordinator.
 protoc -I=. --go_out=plugins=grpc:.  cloud-algos.proto
 python -m grpc_tools.protoc -I=. --python_out=. --grpc_python_out=. cloud-algos.proto
 
-# Compile cloud-connector.proto
-protoc -I=. --go_out=plugins=grpc:.  cloud-connector.proto
-python -m grpc_tools.protoc -I=. --python_out=. --grpc_python_out=. cloud-connector.proto
-
 # Compile site-algos.proto
 protoc -I=. --go_out=plugins=grpc:.  site-algos.proto
 python -m grpc_tools.protoc -I=. --python_out=. --grpc_python_out=. site-algos.proto

f4533fcfe049ebfeec729c1dd006545d3b07572f
Matt <matheusstolet@hotmail.com>
2019-08-16 14:06:51 -0700

    Document leap python utils

--- Utils/leap_utils.py
+++ Utils/leap_utils.py
@@ -1,5 +1,10 @@
 import inspect
 
+
+# Transforms a function to an empty string if its value is None,
+# else it returns the stringified source code for this function.
+#
+# fn: The function to be stringifed.
 def fn_to_string(fn):
     stringified_fn = "" if fn is None else inspect.getsource(fn)
     return stringified_fn

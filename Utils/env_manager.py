5cef40d5fdfb6e2d3827fac80d9a45a7e6e79727
Matt <matheusstolet@hotmail.com>
2019-08-16 14:04:51 -0700

    Document environment manager

--- Utils/env_manager.py
+++ Utils/env_manager.py
@@ -19,15 +19,27 @@ ch.setLevel(logging.DEBUG)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-""" Any commonalities between site and cloud environments
-"""
-class Environment():
+
+# Base environment class. The environment class loads the
+# appropriate imports, state, function and variables. This
+# allows the functions, state, and imports to be accessed
+# inside the process dealing with the requests from a client.
+class Environment:
     pass
 
+
+# Environment class for a site. Loads the imports, state and
+# functions that run inside a site.
 class SiteEnvironment(Environment):
+
     def __init__(self):
         self.logger = logger.withFields({"node": "site-algo"})
 
+    # Loads the general imports that every site needs.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
         import numpy as np
         context["np"] = np
@@ -36,7 +48,16 @@ class SiteEnvironment(Environment):
         self.logger.withFields({"request-id": req_id}).info("Loaded base site environment variables")
 
 
+# Environment class for user defined functions. Loads the
+# imports, state and functions that run in a site.
 class SiteUDFEnvironment(SiteEnvironment):
+
+    # Loads the map functions, the choice function, and the
+    # dataprep function in a site.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
         super().set_env(context, req, req_id)
 
@@ -47,11 +68,21 @@ class SiteUDFEnvironment(SiteEnvironment):
 
         self.logger.withFields({"request-id": req_id}).info("Loaded site environment variables for udf function.")
 
+
+# Loads the appropriate variables and functions for running
+# a differentially private computation in a site using the
+# exponential mechanism.
 class SiteExponentialUDFEnvironment(SiteUDFEnvironment):
+
+    # Loads the score functions, choice function and dataprep
+    # function, along with the functions loaded by SiteUDFEn-
+    # vironment, into the appropriate context.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
         super(SiteUDFEnvironment, self).set_env(context, req, req_id)
-
-        # super().super().set_env(context, req, req_id)
 
         exec(req["score_fns"], context, globals())
         context["score_fn"] = score_fns()
@@ -61,7 +92,17 @@ class SiteExponentialUDFEnvironment(SiteUDFEnvironment):
         self.logger.withFields({"request-id": req_id}).info("Loaded site environment variables for udf function.")
 
 
+# Environment class for loading the appropriate variables
+# and functions for running a predefined algorithm in a site.
 class SitePredefinedEnvironment(SiteEnvironment):
+
+    # Loads the choice, dataprep, and algo code, along with the
+    # functions loaded by SiteEnvironment, into the appropriate
+    # context.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
         super().set_env(context, req, req_id)
         algo_code = req["algo_code"]
@@ -73,13 +114,21 @@ class SitePredefinedEnvironment(SiteEnvironment):
         
         self.logger.withFields({"request-id": req_id}).info("Loaded site environment variables for predefined function.")
 
-
+# Environment class for loading the appropriate variables
+# and functions for running differentially private algorithms
+# in a site.
 class SitePrivatePredefinedEnvironment(SitePredefinedEnvironment):
+
+    # Loads the epsilon and delta values, along with the
+    # parameters defined by the SitePredefinedEnvironment,
+    # into the appropriate context.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
-        ### Privacy specific imports 
         import CloudAlgo.functions.privacy as leap_privacy
         context["leap_privacy"] = leap_privacy
-        ###
         epsilon = req["epsilon"]
         delta = req["delta"]
         privacy_params = {
@@ -90,12 +139,21 @@ class SitePrivatePredefinedEnvironment(SitePredefinedEnvironment):
         super().set_env(context, req, req_id)
 
 
+# Environment class for loading the appropriate variables and functions
+# for running federated learning in a site.
 class SiteFederatedLearningEnvironment(SitePredefinedEnvironment):
+
+    # Loads the model, optimizer and criterion into the context.
+    # Also imports the necessary libraries for federated
+    # learning.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
         algo_code = req["algo_code"]
         module = getattr(leap_fn, algo_code)
 
-        ### Fn specific imports
         import torch
         globals()["torch"] = torch
         context["torch"] = torch
@@ -116,10 +174,19 @@ class SiteFederatedLearningEnvironment(SitePredefinedEnvironment):
         self.logger.withFields({"request-id": req_id}).info("Loaded site environment variables for federated learning.")
 
 
+# Environment class for the cloud. Loads the libraries, functions,
+# and variables that run in the cloud.
 class CloudEnvironment(Environment):
+
     def __init__(self):
         self.logger = logger.withFields({"node": "cloud-algo"})
 
+    # Imports the libraries necessary to run algorithms in the
+    # cloud.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
         import json
         context["json"] = json
@@ -128,7 +195,17 @@ class CloudEnvironment(Environment):
         self.logger.withFields({"request-id": req_id}).info("Loaded base cloud environment variables.")
 
 
+# Extends the cloud environment and loads the appropriate
+# parameters for running user defined algorithms in the cloud.
 class CloudUDFEnvironment(CloudEnvironment):
+
+    # Loads the choice, stop, post-
+    # processing, update and aggregate functions from a user into
+    # the appropriate context in the cloud.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
         super().set_env(context, req, req_id)
 
@@ -145,7 +222,19 @@ class CloudUDFEnvironment(CloudEnvironment):
         self.logger.withFields({"request-id": req_id}).info("Loaded cloud environment variables for udf function.")
     
 
+# Extends the cloud environment and loads the appropriate
+# parameters for running user predefined algorithms in the
+# cloud.
 class CloudPredefinedEnvironment(CloudEnvironment):
+
+    # Loads the choice, stop, post-
+    # processing, update and aggregate functions from a
+    # predefined library into the appropriate context in
+    # the cloud.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
         super().set_env(context, req, req_id)
         algo_code = req["algo_code"]
@@ -161,12 +250,20 @@ class CloudPredefinedEnvironment(CloudEnvironment):
         self.logger.withFields({"request-id": req_id}).info("Loaded cloud environment variables for predefined function.")
 
 
+# Extends the cloud predefined environment and loads the
+# appropriate parameters for running private predefined
+# algorithms.
 class CloudPrivatePredefinedEnvironment(CloudPredefinedEnvironment):
+
+    # Loads the epsilon and the delta into the appropriate
+    # context for the cloud.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
-        ### Privacy specific imports 
         import CloudAlgo.functions.privacy as leap_privacy
         context["leap_privacy"] = leap_privacy
-        ###
 
         epsilon = req["epsilon"]
         delta = req["delta"]
@@ -174,15 +271,25 @@ class CloudPrivatePredefinedEnvironment(CloudPredefinedEnvironment):
             "epsilon": epsilon,
             "delta": delta
         }
+
         context["privacy_params"] = privacy_params
         super().set_env(context, req, req_id)
 
 
+# Extends the CloudPredefinedEnvironment but also loads the
+# the parameters necessary to run a federated learning algorithm
+# in the cloud.
 class CloudFedereatedLearningEnvironment(CloudPredefinedEnvironment):
+
+    # Loads the optimizer, model, criterion and imports Pytorch
+    # into the appropriate context in the cloud.
+    #
+    # context: The context where the imports are loaded to.
+    # req: The request containing the functions to be loaded.
+    # req_id: The id of this request. Used for logging.
     def set_env(self, context, req, req_id):
         super().set_env(context, req, req_id)
 
-        ### Function specific imports
         import torch
         globals()["torch"] = torch
         context["torch"] = torch
@@ -196,6 +303,3 @@ class CloudFedereatedLearningEnvironment(CloudPredefinedEnvironment):
         env_utils.load_from_fn_generator("get_optimizer", "optimizer", req, context, gen_fn_args=[params, hyperparams])
         env_utils.load_from_fn_generator("get_criterion", "criterion", req, context, gen_fn_args=[hyperparams])
         self.logger.withFields({"request-id": req_id}).info("Loaded cloud environment variables for federated learning.")
-
-
-        
--- Utils/env_utils.py
+++ Utils/env_utils.py
@@ -1,8 +1,15 @@
 import inspect
 import pdb
 
-# Loads a function named fn_name to context 
-# from req if it exists, else from module
+# Loads a function named fn_name from req into the appropriate
+# context. If it doesn't exist, load function from pre-existing
+# module.
+#
+# fn_name: The name of the function we want to load.
+# req: The request containing the function to be loaded.
+# context: The context where the function will be loaded to.
+# module: The module containing the predefined implementation
+#         of the function.
 def load_fn(fn_name, req, context, module=None):
     if req[fn_name] == "" and module is not None:
         # If function is not defined
@@ -11,7 +18,16 @@ def load_fn(fn_name, req, context, module=None):
         exec(req[fn_name], context)
         # exec(req[fn_name], context)
 
-# Loads a function from a function generator that returns a list of functions
+# Loads a function from function generator, where the function
+# generator is a list of functions.
+#
+# gen_fn_name: The name of the function generator.
+# fn_name: The name of the list of functions we want to load.
+# req: The request containing the functions to be loaded.
+# context: The context where the functions will be loaded to.
+# module: The module where a predefined function may be loaded
+#         from.
+# gen_fn_args: The arguments that are passed to the funcitons generator.
 def load_from_fn_generator(gen_fn_name, fn_name, req, context, module=None, gen_fn_args=None):
     if req[gen_fn_name] == "" and module is not None:
         exec(inspect.getsource(getattr(module, gen_fn_name)), context, globals())

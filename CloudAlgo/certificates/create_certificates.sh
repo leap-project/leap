93e109b127cf39488fb8545e71b1e00e9d22ec63
Matt <matheusstolet@hotmail.com>
2019-10-28 12:27:51 -0700

    Fix certificate generation scripts

--- CloudAlgo/certificates/create_certificates.sh
+++ CloudAlgo/certificates/create_certificates.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Generate private RSA key to sign and authenticate the public key
-openssl genrsa -out server.key 2048
-# Generate certificate. This is a selg-signed X.509 public key for distribution
-openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650
+sudo openssl genrsa -out cloud_algo.key 2048
+# Generate certificate. This is a self-signed X.509 public key for distribution
+sudo openssl req -new -x509 -sha256 -key cloud_algo.key -out cloud_algo.crt -days 3650
 # Generate a certificate signing request to access the certificate authority
-openssl req -new -sha256 -key server.key -out server.csr
-openssl x509 -req -sha256 -in server.csr -signkey server.key -out server.crt -days 3650
+sudo openssl req -new -sha256 -key cloud_algo.key -out cloud_algo.csr
+sudo openssl x509 -req -sha256 -in cloud_algo.csr -signkey cloud_algo.key -out cloud_algo.crt -days 3650
--- SiteConnector/certificates/create_certificates.sh
+++ SiteConnector/certificates/create_certificates.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Generate private RSA key to sign and authenticate the public key
-opensssl genrsa -out server.key 2048
-# Generate certificate. This is a selg-signed X.509 public key for distribution
-openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650
+sudo openssl genrsa -out siteconn.key 2048
+# Generate certificate. This is a self-signed X.509 public key for distribution
+sudo openssl req -new -x509 -sha256 -key siteconn.key -out siteconn.crt -days 3650
 # Generate a certificate signing request to access the certificate authority
-openssl req -new -sha256 -key server.key -out server.csr
-openssl x509 -req -sha256 -in server.csr -signkey server.key -out server.crt -days 3650
+sudo openssl req -new -sha256 -key siteconn.key -out siteconn.csr
+sudo openssl x509 -req -sha256 -in siteconn.csr -signkey siteconn.key -out siteconn.crt -days 3650

e88da9ff9c5eb6bb16b555b246cc04e0bf2da219
Matt <matheusstolet@hotmail.com>
2020-02-10 22:51:34 -0800

    Add django backend

---
+++ webapp/backend/restserver/views.py
@@ -0,0 +1,23 @@
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class ComputeView(APIView):
+    """
+    API endpoint that sends requests from the web portal to
+    the LEAP infrastructure.
+    """
+    def post(self, request, format=None):
+        print("Got POST for compute")
+        return Response()
+
+class SitesView(APIView):
+    """
+    API endpoint that gets information about the sites registered
+    in LEAP.
+    """
+
+    def get(self, request, format=None):
+        print("Got GET for sites")
+        return Response()

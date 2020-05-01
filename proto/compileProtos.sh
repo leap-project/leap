21d1209ed30473185dbb692b18e5e17b2649ef6a
Matt <matheusstolet@hotmail.com>
2020-05-01 11:41:26 -0700

    remove proto for js and turn off tls

--- proto/compileProtos.sh
+++ proto/compileProtos.sh
@@ -3,7 +3,6 @@
 # Compile computation-msgs.proto
 protoc -I=. --go_out=.  computation-msgs.proto
 protoc -I=. --python_out=. computation-msgs.proto
-protoc -I=. --js_out=import_style=commonjs:../frontend/src/protos computation-msgs.proto
 
 # Compile registration-msgs.proto
 protoc -I=. --go_out=. registration-msgs.proto
@@ -16,7 +15,6 @@ python -m grpc_tools.protoc -I=. --python_out=. --grpc_python_out=. coordinator.
 # Compile cloud-algos.proto
 protoc -I=. --go_out=plugins=grpc:.  cloud-algos.proto
 python -m grpc_tools.protoc -I=. --python_out=. --grpc_python_out=. cloud-algos.proto
-protoc -I=. --grpc-web_out=import_style=commonjs,mode=grpcwebtext:../frontend/src/protos cloud-algos.proto
 
 # Compile site-algos.proto
 protoc -I=. --go_out=plugins=grpc:.  site-algos.proto

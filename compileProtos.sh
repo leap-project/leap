6cae746059fb3fc52b5fb3d7916f7d06aff0e3fc
Matt <matheusstolet@hotmail.com>
2020-05-07 16:38:56 -0700

    Add availability grpc to protos

--- compileProtos.sh
+++ compileProtos.sh
@@ -8,6 +8,10 @@ protoc -I=.  --python_out=.  proto/computation-msgs.proto
 protoc -I=.  --go_out=proto/  proto/registration-msgs.proto
 protoc -I=.  --python_out=.  proto/registration-msgs.proto
 
+# Compile availability-msgs.proto
+protoc -I=.  --go_out=proto/  proto/availability-msgs.proto
+protoc -I=.  --python_out=.  proto/availability-msgs.proto
+
 # Compile coordinator.proto
 protoc -I=. --go_out=plugins=grpc:proto/ proto/coordinator.proto
 python -m grpc_tools.protoc -I=. --python_out=. --grpc_python_out=. proto/coordinator.proto

a4db291c251cd264c0e8bee6d9af7311188467dd
Matheus <matheusstolet@hotmail.com>
2021-02-17 22:40:36 -0800

    Add pull all

--- evals/utils/pull-all.sh
+++ evals/utils/pull-all.sh
@@ -1,17 +1,42 @@
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 5 ]; then
     echo "Illegal number of parameters (expecting 3):"
-    echo "[ipfile, uname, branchname]"
+    echo "[ipfile-sites, ipfile-client, ipfile-cloud, uname, branchname]"
     exit
 fi
 
-ipfile=$1
-uname=$2
-branchname=$3
+ipfile_sites=$1
+ipfile_client=$2
+ipfile_cloud=$3
+uname=$4
+branchname=$5
 
 leapPath="/home/$uname/gopath/src/leap"
-hostFile="../azure-conf/$ipfile"
 
-for ip in $(cat $hostFile);do
+for ip in $(cat $ipfile_sites);do
+
+	ssh -t $uname@$ip "
+		cd $leapPath
+		git stash
+		git fetch origin
+		git checkout $branchname
+		git pull origin $branchname
+	"
+
+done
+
+for ip in $(cat $ipfile_client);do
+
+	ssh -t $uname@$ip "
+		cd $leapPath
+		git stash
+		git fetch origin
+		git checkout $branchname
+		git pull origin $branchname
+	"
+
+done
+
+for ip in $(cat $ipfile_cloud);do
 
 	ssh -t $uname@$ip "
 		cd $leapPath

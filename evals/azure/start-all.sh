90a097bfd1f1f79c58cf73e0667fb68accb27d46
Matheus <matheusstolet@hotmail.com>
2021-03-08 16:44:24 -0800

    Specify number of sites to start on azure

--- evals/azure/start-all.sh
+++ evals/azure/start-all.sh
@@ -1,3 +1,11 @@
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters (expecting 1):"
+    echo "[num_sites]"
+    exit
+fi
+
+n_sites=$1
+
 bash start-client.sh
-bash start-sites.sh
+bash start-sites.sh $n_sites
 bash start-cloud.sh
--- evals/azure/start-sites.sh
+++ evals/azure/start-sites.sh
@@ -1,6 +1,18 @@
 echo "Starting sites..."
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters (expecting 1):"
+    echo "[num_sites]"
+    exit
+fi
+
+n_sites=$1
+
 for i in {1..15}
 do
     echo "Starting site.${i}:"
     bash start-vm.sh Leap site.${i}
+
+    if [ $i -eq $n_sites ]; then
+      break
+    fi
 done
